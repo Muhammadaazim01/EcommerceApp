de4329528f6c916fbf442213113131a75a358c3b
Muhammadaazim01 <muhammadaazim96@gmail.com>
2025-08-03 06:08:32 +0500

    addmyapp

--- lib/Screens/cart/thankyou_for_shopping.dart
+++ lib/Screens/cart/thankyou_for_shopping.dart
@@ -2,7 +2,6 @@ import 'package:ecommerceapp/Widgets/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
 
 class ThankYouScreen extends StatefulWidget {
   const ThankYouScreen({super.key});
